a65ad6f99ed4dc429831ee42d1007f7903e61934
Grupo Rojo <80834743+GrupoRojo@users.noreply.github.com>
2021-04-23 11:44:15 -0500

    Add files via upload

---
+++ ClienteWeb/cbs/estilo.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+	$('.ima1, .ima2, .ima3').hover(function () {
+		$(this).animate({ marginTop: '-=20px' }, 200);
+	}, function () {
+			$(this).animate({ marginTop: '+=20px' }, 200);
+	});
+});
